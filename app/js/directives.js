ac1ebb96638ed5be9eb0c38f5439f74a906e31f7
farolfo <francoarolfo@hotmail.com>
2014-02-17 17:40:19 -0300

    made some magic

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,55 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+  directive('faHarmonica', function() {
+        return {
+
+            restrict: 'E',
+
+            templateUrl: './partials/directives/faHarmonica.html',
+
+            scope: {
+                mainKey: '='
+            },
+
+            link: function (scope, elem, attrs) {
+                scope.cells = [1,2,3,4,5,6,7,8,9,10];
+            }
+        };
+  }).
+
+    directive('faHarmonicaCell', ['HarmonicaService', function(HarmonicaService) {
+        return {
+
+            restrict: 'E',
+
+            templateUrl: './partials/directives/faHarmonicaCell.html',
+
+            scope: {
+                number: '=',
+                mainKey: '='
+            },
+
+            link: function (scope, elem, attrs) {
+                scope.notes = HarmonicaService.getNotes(scope.mainKey, scope.number);
+            }
+        };
+    }]).
+    directive('faHarmonicaNote', function() {
+        return {
+
+            restrict: 'E',
+
+            templateUrl: './partials/directives/faHarmonicaNote.html',
+
+            scope: {
+                index: '=',
+                key: '=',
+                type: '='
+            },
+
+            link: function (scope, elem, attrs) {
+
+            }
+        };
+    });
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,110 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  value('version', '0.1')
+    .service('HarmonicaService', function () {
+
+        var harmonicas = {
+            'C': [
+                {
+                    blow: [
+                        {key: 'C'}
+                    ],
+                    draw: [
+                        {key: 'D'},
+                        {key: 'C#'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'E'}
+                    ],
+                    draw: [
+                        {key: 'G'},
+                        {key: 'F#'},
+                        {key: 'F'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'G'}
+                    ],
+                    draw: [
+                        {key: 'B'},
+                        {key: 'Bb'},
+                        {key: 'A'},
+                        {key: 'Ab'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'C'}
+                    ],
+                    draw: [
+                        {key: 'D'},
+                        {key: 'C#'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'E'}
+                    ],
+                    draw: [
+                        {key: 'F'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'G'}
+                    ],
+                    draw: [
+                        {key: 'A'},
+                        {key: 'Ab'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'C'}
+                    ],
+                    draw: [
+                        {key: 'B'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'E'},
+                        {key: 'Eb'}
+                    ],
+                    draw: [
+                        {key: 'D'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'G'},
+                        {key: 'F#'}
+                    ],
+                    draw: [
+                        {key: 'F'}
+                    ]
+                },
+                {
+                    blow: [
+                        {key: 'C'},
+                        {key: 'B'},
+                        {key: 'Bb'}
+                    ],
+                    draw: [
+                        {key: 'A'}
+                    ]
+                }
+            ]
+        };
+
+        return {
+            getNotes: function(mainKey, cellNumber) {
+                return harmonicas[mainKey][cellNumber-1];
+            }
+        };
+    });
+
